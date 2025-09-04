3516315e36f9ef6e04e04bf077ccdd4a34b12140
Julia Amorim <julial.brito@uni9.edu.br>
2025-09-04 00:03:45 +0000

    feat: if statement

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+void main(){
+    int idade = 18;
+    if(idade >= 18)
+    {
+        print('pode entrar na balada');
+    }
+    print('fim do programa');
+}
+import 'dart:io';
+void main(){
+    int idade = 18;
+    if(idade >= 18)
+    {
+        print('pode entrar na balada');
+    }
+    print('fim do programa');
+}
